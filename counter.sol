d3672976cd69f543ef9dfcbd48ccef84da6f2b63
Taha Ozturk <thoztrk007@gmail.com>
2023-01-02 22:21:56 +0300

    aciklama satiri eklendi

--- counter.sol
+++ counter.sol
@@ -4,11 +4,13 @@ pragma solidity ^0.8.7;
 contract counter {
     uint public  count;
 
+// Function to increment count by 1
 function inc() external {
     count +=1;
 }
 
+// Function to decrement count by 1
 function dec() external {
-    count -=1;
+    count -=1; // This function will fail if count = 0
 }
 }
